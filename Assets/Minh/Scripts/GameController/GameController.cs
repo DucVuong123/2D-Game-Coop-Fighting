92022999f59af971d35d817cf02842285daf217f
DoVanMinh234 <21a100100235@students.hou.edu.vn>
2025-11-21 20:25:05 +0700

    cmt

--- Assets/Minh/Scripts/GameController/GameController.cs
+++ Assets/Minh/Scripts/GameController/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -16,9 +17,12 @@ public class GameController : MonoBehaviour
     private Dictionary<string, GameObject> Chars;
 
     [Header("MainPlayers")]
-    public GameObject Player_Choice;
+    public GameObject Player_Choice_Char;
     public GameObject Main_Player;
 
+
+    [Header("Account_Player")]
+    public GameDataModels.NguoiChoi Main_Player_Acc;
 
     public static GameController Instance;
 
@@ -52,7 +56,7 @@ public class GameController : MonoBehaviour
         }
         if (currentMap != null) Destroy(currentMap);
         currentMap = Instantiate(Maps[currentMapIndex = currentMapIndex < Maps.Count - 1 ? currentMapIndex + 1 : 0], Vector2.zero, Quaternion.identity);
-        if (Main_Player == null) { Main_Player = Instantiate(Player_Choice, currentMap.GetComponent<MapController>().Instance.Poss_Player.position, Quaternion.identity); return; };
+        if (Main_Player == null) { Main_Player = Instantiate(Player_Choice_Char, currentMap.GetComponent<MapController>().Instance.Poss_Player.position, Quaternion.identity); return; };
         Main_Player.transform.position = currentMap.GetComponent<MapController>().Instance.Poss_Player.position;
         
     }
@@ -63,11 +67,11 @@ public class GameController : MonoBehaviour
         switch (_Charactor)
         {
             case DuKichMovement:
-                Player_Choice = Chars["Du_Kich"];
+                Player_Choice_Char = Chars["Du_Kich"];
                 break;
 
             case BoBinhMovement:
-                Player_Choice = Chars["Bo_Binh"];
+                Player_Choice_Char = Chars["Bo_Binh"];
                 break;
         }
     }
@@ -83,9 +87,56 @@ public class GameController : MonoBehaviour
     {
         // Hủy đăng ký event để không bị gọi lại nhiều lần
         SceneManager.sceneLoaded -= OnSceneLoaded_InGame;
-        if (Main_Player == null) Main_Player = Instantiate(Player_Choice,Vector2.zero, Quaternion.identity);
+        if (Main_Player == null) Main_Player = Instantiate(Player_Choice_Char, Vector2.zero, Quaternion.identity);
         nextMap();
 
 
     }
+
+
+
+    public bool check_Account_Players_AfterLogin(Account acc)
+    {
+
+        Main_Player_Acc = new GameDataModels.NguoiChoi();
+        // Lấy dữ liệu người chơi từ Firebase
+        DatabaseCtr.Instance.GetData<GameDataModels.NguoiChoi>($"NguoiChoi/{acc.accountId}", (success, data, message) =>
+        {
+            if (success && data != null)
+            {
+                // Người chơi đã tồn tại
+                Main_Player_Acc = data;
+                Debug.Log($"Người chơi đã tồn tại: {data.TenNguoiChoi}");
+               
+            }
+            else
+            {
+                Debug.Log("Người chơi là người chơi mới");
+             /*   DatabaseCtr.Instance.GenerateUniquePlayerId(uniqueId =>
+                {
+                    GameDataModels.NguoiChoi newPlayer = new GameDataModels.NguoiChoi
+                    {
+                        MaNguoiChoi = uniqueId,
+                        TenNguoiChoi = acc.username,
+                        CapDo = 1,
+                        TrangThai = "Online",
+                        MaTranDau = "",
+                        MaTaiKhoan = acc.accountId
+                    };
+
+                    DatabaseCtr.Instance.AddData($"NguoiChoi/{acc.accountId}", newPlayer, (addSuccess, addMessage) =>
+                    {
+                        if (addSuccess)
+                            Debug.Log("Người chơi mới đã được tạo!");
+                        else
+                            Debug.LogError("Tạo người chơi mới thất bại: " + addMessage);
+                    });
+                });*/
+            }
+        });
+
+
+        return Main_Player_Acc != null;
+    }
+
 }
